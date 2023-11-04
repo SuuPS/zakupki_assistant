e6132e9707f2c7ba9582e0a3e6e86d41885078a3
jusup <jusup.munanbekuulu@gmail.com>
2023-11-04 17:11:40 +0600

    Регистрация

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -156,7 +156,7 @@ const Header = () => {
                         <ThemeToggler/>
 
                         <Link
-                            href="https://nextjstemplates.com/templates/solid"
+                            href="/auth/signin"
                             className="flex items-center justify-center rounded-full bg-primary px-7.5 py-2.5 text-regular text-white duration-300 ease-in-out hover:bg-primaryho">
                             <span className="xs:pr-2"><svg width="18" height="18" viewBox="0 0 18 18" fill="none"
                                                            xmlns="http://www.w3.org/2000/svg"><path
